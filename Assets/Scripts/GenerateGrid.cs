fb7a16ee3a4f8145bc2e7cddbcfdf01967080431
Bochdan <szymon@windekor.pl>
2018-04-30 20:55:41 +0200

    Initial

---
+++ Assets/Scripts/GenerateGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGrid : MonoBehaviour {
+
+    public Transform gridPrefab;
+    public int size;
+
+	// Use this for initialization
+	void Start () {
+        MakeGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void MakeGrid()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            var xpos = size * i;
+            var vex = Vector3.zero;
+            vex.x = xpos;
+            Instantiate(gridPrefab, vex, gridPrefab.rotation);
+        }
+    }
+}
